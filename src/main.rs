4d7f36f76b4dc6add0abbfddf324eeea5cc0e846
Alex Kochurov <alex.kochurov@gmail.com>
2025-03-05 03:12:00 +0300

    add decode function. The version becomes 0.91

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,8 @@ use aes_gcm::aead::{Aead, NewAead, generic_array::GenericArray};
 use aes_gcm::Aes256Gcm;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Usage: cargo run -- encode OR cargo run -- decode
-    // let args: Vec<String> = env::args().collect();
-
-    encode()?;
+    // encode()?;
+    decode()?;
     Ok(())
 }
 
@@ -27,36 +25,36 @@ fn read_password() -> Result<String, Box< dyn std::error::Error>> {
     Ok(contents.trim().to_string())
 }
 
+fn generate_cipher(salt: &[u8]) -> Result<Aes256Gcm, Box< dyn std::error::Error>> {
+    let password = read_password()?;
+
+    let argon2 = Argon2::default();
+    let mut key = [0u8; 32];
+    argon2
+        .hash_password_into(password.as_bytes(), salt, &mut key)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+
+    let cipher = Aes256Gcm::new(GenericArray::from_slice(&key));
+
+    Ok(cipher)
+}
+
 #[allow(dead_code)]
 fn encode() -> Result<(), Box<dyn std::error::Error>> {
-    let password = read_password()?;
-    // println!("You placed the \"{}\" password in the file", password);
-
     let mut source = File::open("source_file")?;
     let mut plaintext = Vec::new();
     source.read_to_end(&mut plaintext)?;
 
     let mut salt = [0u8; 16];
-    // let mut rng = OsRng::default();
     OsRng.fill_bytes(&mut salt);
-
-    let argon2 = Argon2::default();
-    let mut key = [0u8; 32];
-    
-    argon2
-        .hash_password_into(password.as_bytes(), &salt, &mut key)
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
-
-    // println!("Generated key: {:?}", &key);
 
     let mut nonce = [0u8; 12];
     OsRng.fill_bytes(&mut nonce);
 
-    let cipher = Aes256Gcm::new(GenericArray::from_slice(&key));
+    let cipher = generate_cipher(&salt)?;
 
     let ciphertext = cipher.encrypt(GenericArray::from_slice(&nonce), plaintext.as_ref())
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
-        // .expect("encryption failure!");
 
     let mut out_file = File::create("box")?;
     out_file.write_all(&salt)?;
@@ -70,8 +68,24 @@ fn encode() -> Result<(), Box<dyn std::error::Error>> {
 
 #[allow(dead_code)]
 fn decode() -> Result<(), Box<dyn std::error::Error>> {
-    let password = read_password()?;
-
     let mut file = File::open("box")?;
     let mut data = Vec::new();
+    file.read_to_end(&mut data)?;
+
+    if data.len() < 28 {
+        return Err("The 'box' file is too short to contain necessary encryption parameters.".into());
+    }
+
+    let salt = &data[0..16];
+    let nonce = &data[16..28];
+    let ciphertext = &data[28..];
+
+    let cipher = generate_cipher(salt)?;
+
+    let plaintext = cipher.decrypt(GenericArray::from_slice(nonce), ciphertext.as_ref())
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+
+    println!("{}", String::from_utf8_lossy(&plaintext));
+
+    Ok(())
 }
